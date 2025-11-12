7907f7bc30213e79d6158363007fbca33bf5f729
tbodyowski <tom.ziese@icloud.com>
2025-11-12 17:15:42 +0100

    Cleared Main.java

--- src/main/java/de/tbodyowski/pureos/Main.java
+++ src/main/java/de/tbodyowski/pureos/Main.java
@@ -40,53 +40,25 @@ public final class Main extends JavaPlugin implements Listener {
     private String Status_Prefix = "";
     private Boolean DeathCounter_on_off = (Boolean) false;
     private static Main instance;
-    private NamespacedKey invisibleRecipe;
-    private static NamespacedKey invisibleKey;
-    private Set<DroppedFrameLocation> droppedFrames;
-    private boolean framesGlow;
-    private boolean firstLoad = true;
     private PrefixManager prefixManager;
     private FileManager fileManager;
-    private Material glowInkSac = null;
-    private Material glowFrame = null;
-    private EntityType glowFrameEntity = null;
     private GhastSpeedInventory ghastSpeedInventory;
 
     private ConfigVarManager configVarManager;
     File configFile = new File(getDataFolder(), "config.yml");
-    private File EggLocation;
-    private FileConfiguration eggConfig;
 
 
 
     @Override
     public void onEnable() {
         if (!configFile.exists()) saveDefaultConfig();
-        EggLocation = new File(getDataFolder(), "egglocation.yml");
-        if (!EggLocation.exists()){
-            try {
-                EggLocation.createNewFile();
-            }catch (IOException e){
-                e.printStackTrace();
-            }
-        }
-        eggConfig = YamlConfiguration.loadConfiguration(EggLocation);
         instance = this;
         this.prefixManager = new PrefixManager();
         this.configVarManager = new ConfigVarManager();
         this.fileManager = new FileManager();
         this.ghastSpeedInventory = new GhastSpeedInventory();
-        invisibleRecipe = new NamespacedKey(this, "invisible-recipe");
-        invisibleKey = new NamespacedKey(this, "invisible");
         configVarManager.updateVar();
 
-        droppedFrames = new HashSet<>();
-        try {
-            glowInkSac = Material.valueOf("GLOW_INK_SAC");
-            glowFrame = Material.valueOf("GLOW_ITEM_FRAME");
-            glowFrameEntity = EntityType.valueOf("GLOW_ITEM_FRAME");
-        } catch (IllegalArgumentException ignored) {
-        }
         reload();
         getLogger().info("WarOS is starting up...");
 
@@ -119,37 +91,12 @@ public final class Main extends JavaPlugin implements Listener {
     public void onDisable() {
         getLogger().info("WarOS is shutting down...");
         saveConfig();
-        removeRecipe();
         if (this.getFileManager() != null) {
             this.getFileManager().saveStatusFile();
             this.getFileManager().saveBlockedWordsFile();
         } else {
             getLogger().warning("FileManager is null during shutdown. Skipping file save.");
         }
-    }
-
-    private void removeRecipe() {
-        Iterator<Recipe> iter = getServer().recipeIterator();
-        while (iter.hasNext()) {
-            Recipe check = iter.next();
-            if (isInvisibleRecipe(check)) {
-                iter.remove();
-                break;
-            }
-        }
-
-    }
-
-    public Boolean getDeathCounter_on_off() {
-        return DeathCounter_on_off;
-    }
-
-    public FileConfiguration getEggConfig() {
-        return eggConfig;
-    }
-
-    public File getEggLocation() {
-        return EggLocation;
     }
 
     public static Main getInstance() {
@@ -168,228 +115,11 @@ public final class Main extends JavaPlugin implements Listener {
         return Status_Prefix;
     }
 
-    public void setRecipeItem(ItemStack item) {
-        getConfig().set("recipe", item);
-        saveConfig();
-        reload();
-    }
-
     public void reload() {
         saveConfig();
         reloadConfig();
         getConfig().options().copyDefaults(true);
         saveConfig();
-        removeRecipe();
-
-        if (firstLoad) {
-            firstLoad = false;
-            framesGlow = !getConfig().getBoolean("item-frames-glow");
-        }
-        if (getConfig().getBoolean("item-frames-glow") != framesGlow) {
-            framesGlow = getConfig().getBoolean("item-frames-glow");
-            forceRecheck();
-        }
-
-        ItemStack invisibleItem = generateInvisibleItemFrame();
-        invisibleItem.setAmount(8);
-
-        ItemStack invisibilityPotion = getConfig().getItemStack("recipe");
-        ShapedRecipe invisRecipe = new ShapedRecipe(invisibleRecipe, invisibleItem);
-        invisRecipe.shape("FFF", "FPF", "FFF");
-        invisRecipe.setIngredient('F', Material.ITEM_FRAME);
-        invisRecipe.setIngredient('P', Material.GLOWSTONE_DUST);
-        Bukkit.addRecipe(invisRecipe);
-    }
-
-    public void forceRecheck() {
-        for (World world : Bukkit.getWorlds()) {
-            for (ItemFrame frame : world.getEntitiesByClass(ItemFrame.class)) {
-                if (frame.getPersistentDataContainer().has(invisibleKey, PersistentDataType.BYTE)) {
-                    if (frame.getItem().getType() == Material.AIR && framesGlow) {
-                        frame.setGlowing(true);
-                        frame.setVisible(true);
-                    } else if (frame.getItem().getType() != Material.AIR) {
-                        frame.setGlowing(false);
-                        frame.setVisible(false);
-                    }
-                }
-            }
-        }
-    }
-
-    private boolean isInvisibleRecipe(Recipe recipe) {
-        return (recipe instanceof ShapedRecipe && ((ShapedRecipe) recipe).getKey().equals(invisibleRecipe));
-    }
-
-    private boolean isFrameEntity(Entity entity) {
-        return (entity != null && (entity.getType() == EntityType.ITEM_FRAME ||
-                (glowFrameEntity != null && entity.getType() == glowFrameEntity)));
-    }
-
-    public static ItemStack generateInvisibleItemFrame() {
-        ItemStack item = new ItemStack(Material.ITEM_FRAME, 1);
-        ItemMeta meta = item.getItemMeta();
-        meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
-        meta.addEnchant(Enchantment.UNBREAKING, 1, true);
-        meta.setDisplayName(ChatColor.WHITE + "Invisible Item Frame");
-        meta.getPersistentDataContainer().set(invisibleKey, PersistentDataType.BYTE, (byte) 1);
-        item.setItemMeta(meta);
-        return item;
-    }
-
-    @EventHandler(ignoreCancelled = true)
-    private void onCraft(PrepareItemCraftEvent event) {
-        if (isInvisibleRecipe(event.getRecipe()) && !event.getView().getPlayer().hasPermission("survivalinvisiframes.craft")) {
-            event.getInventory().setResult(null);
-        } else if (glowInkSac != null && glowFrame != null) {
-            boolean foundFrame = false;
-            boolean foundInkSac = false;
-            for (ItemStack i : event.getInventory().getMatrix()) {
-                if (i == null || i.getType() == Material.AIR) continue;
-
-                if (i.getType() == glowInkSac) {
-                    if (foundInkSac) return;
-                    foundInkSac = true;
-                    continue;
-                }
-
-                if (i.getItemMeta().getPersistentDataContainer().has(invisibleKey, PersistentDataType.BYTE) &&
-                        i.getType() != glowFrame) {
-                    if (foundFrame) return;
-                    foundFrame = true;
-                    continue;
-                }
-                return;
-            }
-
-            if (foundFrame && foundInkSac && event.getView().getPlayer().hasPermission("survivalinvisiframes.craft")) {
-                ItemStack invisibleGlowingItem = generateInvisibleItemFrame();
-                ItemMeta meta = invisibleGlowingItem.getItemMeta();
-                meta.setDisplayName(ChatColor.WHITE + "Glow Invisible Item Frame");
-                invisibleGlowingItem.setItemMeta(meta);
-                invisibleGlowingItem.setType(glowFrame);
-
-                event.getInventory().setResult(invisibleGlowingItem);
-            }
-        }
-    }
-
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
-    private void onHangingPlace(HangingPlaceEvent event) {
-        if (!isFrameEntity(event.getEntity()) || event.getPlayer() == null) {
-            return;
-        }
-
-        ItemStack frame;
-        Player p = event.getPlayer();
-        if (p.getInventory().getItemInMainHand().getType() == Material.ITEM_FRAME ||
-                (glowFrame != null && p.getInventory().getItemInMainHand().getType() == glowFrame)) {
-            frame = p.getInventory().getItemInMainHand();
-        } else if (p.getInventory().getItemInOffHand().getType() == Material.ITEM_FRAME ||
-                (glowFrame != null && p.getInventory().getItemInOffHand().getType() == glowFrame)) {
-            frame = p.getInventory().getItemInOffHand();
-        } else {
-            return;
-        }
-
-        if (frame.getItemMeta().getPersistentDataContainer().has(invisibleKey, PersistentDataType.BYTE)) {
-            if (!p.hasPermission("survivalinvisiframes.place")) {
-                event.setCancelled(true);
-                return;
-            }
-            ItemFrame itemFrame = (ItemFrame) event.getEntity();
-            if (framesGlow) {
-                itemFrame.setVisible(true);
-                itemFrame.setGlowing(true);
-            } else {
-                itemFrame.setVisible(false);
-            }
-            event.getEntity().getPersistentDataContainer().set(invisibleKey, PersistentDataType.BYTE, (byte) 1);
-        }
-    }
-
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
-    private void onHangingBreak(HangingBreakEvent event) {
-        if (!isFrameEntity(event.getEntity()) || !event.getEntity().getPersistentDataContainer().has(invisibleKey, PersistentDataType.BYTE)) {
-            return;
-        }
-
-
-        DroppedFrameLocation droppedFrameLocation = new DroppedFrameLocation(event.getEntity().getLocation());
-        droppedFrames.add(droppedFrameLocation);
-        droppedFrameLocation.setRemoval((new BukkitRunnable() {
-            @Override
-            public void run() {
-                droppedFrames.remove(droppedFrameLocation);
-            }
-        }).runTaskLater(this, 20L));
-    }
-
-    @EventHandler
-    private void onItemSpawn(ItemSpawnEvent event) {
-        Item item = event.getEntity();
-        if (item.getItemStack().getType() != Material.ITEM_FRAME && (glowFrame == null || item.getItemStack().getType() != glowFrame)) {
-            return;
-        }
-
-        Iterator<DroppedFrameLocation> iter = droppedFrames.iterator();
-        while (iter.hasNext()) {
-            DroppedFrameLocation droppedFrameLocation = iter.next();
-            if (droppedFrameLocation.isFrame(item)) {
-                ItemStack frame = generateInvisibleItemFrame();
-                if (glowFrame != null && item.getItemStack().getType() == glowFrame) {
-                    ItemMeta meta = frame.getItemMeta();
-                    meta.setDisplayName(ChatColor.WHITE + "Glow Invisible Item Frame");
-                    frame.setItemMeta(meta);
-                    frame.setType(glowFrame);
-                }
-                event.getEntity().setItemStack(frame);
-
-                droppedFrameLocation.getRemoval().cancel();
-                iter.remove();
-                break;
-            }
-        }
-    }
-
-    @EventHandler(ignoreCancelled = true)
-    private void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
-        if (!framesGlow) {
-            return;
-        }
-
-        if (isFrameEntity(event.getRightClicked()) &&
-                event.getRightClicked().getPersistentDataContainer().has(invisibleKey, PersistentDataType.BYTE)) {
-            ItemFrame frame = (ItemFrame) event.getRightClicked();
-            Bukkit.getScheduler().runTaskLater(this, () ->
-            {
-                if (frame.getItem().getType() != Material.AIR) {
-                    frame.setGlowing(false);
-                    frame.setVisible(false);
-                }
-            }, 1L);
-        }
-    }
-
-    @EventHandler(ignoreCancelled = true)
-    private void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
-        if (!framesGlow) {
-            return;
-        }
-
-        if (isFrameEntity(event.getEntity()) &&
-                event.getEntity().getPersistentDataContainer().has(invisibleKey, PersistentDataType.BYTE)) {
-            ItemFrame frame = (ItemFrame) event.getEntity();
-            Bukkit.getScheduler().runTaskLater(this, () ->
-            {
-                if (frame.getItem().getType() == Material.AIR) {
-                    if (framesGlow) {
-                        frame.setGlowing(true);
-                        frame.setVisible(true);
-                    }
-                }
-            }, 1L);
-        }
     }
     public static void startSaveAndRegisterPlayer() {
         YamlConfiguration statusData = Main.getInstance().getFileManager().getStatusData();
